7824f09b0f344f71dfe847d343a52ff1c16c6f6d
LiuFFT <liufft98@gmail.com>
2022-02-17 18:25:50 +0800

    part12-exercises12.8

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose')
 const Todo = require('./models/Todo')
 const { MONGO_URL } = require('../util/config')
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+if (MONGO_URL && !mongoose.connection.readyState)
+  mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(x => {
+        console.log(
+            `Connected to Mongo! Database name: "${x.connections[0].name}"`,
+        );
+      })
+      .catch(err => {
+        console.error('Error connecting to mongo', err);
+      });
 
 
 module.exports = {
